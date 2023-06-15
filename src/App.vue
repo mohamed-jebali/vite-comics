b7f5851ad7111b7d6ab89ee76c1d50289acb7208
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-06-15 15:19:45 +0200

    added AppHeader

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import './styles/general.scss'
+import AppHeader from './components/AppHeader.vue'
+
+
+export default{
+  components:{
+    AppHeader
+  }
+}
+
+</script>
+
+<template>
+  <AppHeader/>
+</template>
+
+<style lang="scss">
+ul{
+  list-style-type: none;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,51 @@
+<template lang="">
+    <div class='container-header mg-auto'>     
+        <header class="flex">
+            <div class='brand'>
+                <img src="../../public/img/dc-logo.png" alt="dc-logo">
+            </div>
+            <nav class='navbar-header'>
+                <ul class='flex'>
+                    <li v-for="liElements in headerList">{{liElements}}</li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+</template>
+<script>
+import { createApp } from 'vue'
+export default{
+    data(){
+        return{
+            headerList:["CHARACTERS","COMICS","MOVIES","TV","GAMES",,"COLLECTIBLES",,"VIDEOS","FANS","NEWS","SHOP"]
+        }
+    },
+    methods:{
+
+    }
+}
+
+</script>
+<style lang="scss" scoped>
+@use '../styles/partials/mixin' as *; 
+@use '../styles/partials/variables' as *; 
+
+
+.container-header{
+    max-width: 1200px;
+}
+
+header{
+    border:1px solid black;
+    @include flex(row,space-between);
+}
+
+ul{
+    li{
+        @include flex();
+        color : $DcNavColor;
+        margin-right: 1rem;
+    }
+}
+
+</style>
