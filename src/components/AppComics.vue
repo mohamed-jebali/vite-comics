a77cdfba96134a74e0ded8adbc2f050ed8a5e901
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-06-16 15:13:22 +0200

    added list Comics by prop component

---
+++ src/components/AppComics.vue
@@ -0,0 +1,45 @@
+<template>
+    <li>
+        {{ elementComics.series }}
+    </li>
+</template>
+<script>
+import { createApp } from 'vue'
+export default{
+        name:'AppComics',
+    props:{
+        elementComics:String
+    },
+
+    }
+
+</script>
+<style lang="scss" scoped>
+@use '../styles/partials/mixin' as *; 
+@use '../styles/partials/variables' as *;
+
+
+.container-header{
+    max-width: 1000px;
+}
+
+header{
+    @include flex(row,space-between);
+    margin: 1rem 0;
+}
+
+
+ul{
+    li{
+        @include flex();
+        margin-right: 1.5rem;
+        font-size: 0.65rem;
+        font-weight: 600;
+
+        &:hover{
+            text-decoration: underline;
+        }
+    }
+}
+
+</style>
